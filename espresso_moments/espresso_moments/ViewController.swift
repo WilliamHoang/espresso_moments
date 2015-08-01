8c8a1b84f2032320cfc56eb9736c26c06fd2ac5d
WiLL <william.sun.hoang@gmail.com>
2015-08-01 12:11:08 -0400

    first iOS commit

---
+++ espresso_moments/espresso_moments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  espresso_moments
+//
+//  Created by WiLL on 8/1/15.
+//  Copyright (c) 2015 Harvard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
