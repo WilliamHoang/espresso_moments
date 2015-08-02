19b0d249fbc3cfed65d3f5b8a3251d268c0ec2ec
WiLL <william.sun.hoang@gmail.com>
2015-08-02 19:51:25 -0400

    add profile outlet to segue & FB Login-Profile

--- Espresso Moments/Espresso Moments/ProfileViewController.swift
+++ Espresso Moments/Espresso Moments/ProfileViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
+
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var fbLoginView: FBSDKLoginButton!{
+        didSet {
+            fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+        }
+    }
+    @IBOutlet weak var fbProfileView: FBSDKProfilePictureView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +29,32 @@ class ProfileViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let accessToken = FBSDKAccessToken.currentAccessToken() {
+            // user is logged in
+            println("User's ID is \(accessToken.userID)")
+            println("permissions: \(accessToken.permissions)")
+            getFBUserData()
+        } else {
+            // user is not logged in
+        }
+    }
+    
+    func getFBUserData() {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me?fields=id,email,name,friends", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil) {
+                // Process error
+                println("Error: \(error)")
+            } else {
+                println("fetched result: \(result)")
+            }
+        })
     }
     
 
