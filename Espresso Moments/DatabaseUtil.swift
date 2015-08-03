d519f8fb9276241cc390d4e06e4071198664f49f
WiLL <william.sun.hoang@gmail.com>
2015-08-03 00:02:22 -0400

    add Database.Util

---
+++ Espresso Moments/DatabaseUtil.swift
@@ -0,0 +1,22 @@
+//
+//  DatabaseUtil.swift
+//  Espresso Moments
+//
+//  Created by WiLL on 8/2/15.
+//  Copyright (c) 2015 Harvard. All rights reserved.
+//
+
+import Foundation
+
+class DatabaseUtil {
+    class func getEmptyDatabase(name: String!, error: NSErrorPointer) -> CBLDatabase? {
+        if let database = CBLManager.sharedInstance().existingDatabaseNamed(name, error: nil){
+            if !database.deleteDatabase(error){
+                return nil;
+            }
+        }
+        
+        
+        return CBLManager.sharedInstance().databaseNamed(name, error: error)
+    }
+}
